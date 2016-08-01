ca350923fa86ce1c6dbdd3940da9ee86f3d68184
Brian Dawn <brian.t.dawn@gmail.com>
2016-08-01 10:27:36 -0500

    Kill OMF because it is evil, use fisherman instead.

--- .bin/install-stuff.sh
+++ .bin/install-stuff.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles-repo/ --work-tree=$HOME'
 
-curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
-
-# Reset the config directory because oh my fish is evil.
-config checkout .config
 
 # Tmux pkg manager
+rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install emacs pkg management
@@ -16,17 +12,32 @@ curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+###################
+# Fish Management #
+###################
+
 # Install base16 themes for fish
+rm -rf ~/.config/base16-shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 # Install fisher - yet another fish package manager...
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
 # Install fzf - with fish integration...
+rm -rf ~/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-$HOME/.fzf/install
+yes | ~/.fzf/install
 
 # Install fzf fish plugin.
 fish -c fisher fzf
 
+###################
+#     Cleanup     #
+###################
+echo 'Cleanup!'
 
+# Cleanup modifications made by the various installs.
+/usr/bin/git --git-dir=$HOME/.dotfiles-repo/ --work-tree=$HOME checkout ~/.bashrc
+
+rm -rf ~/.config/fish
+/usr/bin/git --git-dir=$HOME/.dotfiles-repo/ --work-tree=$HOME checkout ~/.config/fish
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,3 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH $HOME/.local/share/omf
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 ####################
 # Custom Functions #
 ####################
