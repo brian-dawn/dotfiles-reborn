b0aba59f369b45e41ea2f6967e3c4c26d28f6414
Brian Dawn <brian.t.dawn@gmail.com>
2016-10-19 16:31:41 -0500

    Lets go back to zsh...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -70,7 +70,7 @@ add_to_path $HOME/.local/bin # prefer stack
 
 # Base16 Shell
 if status --is-interactive
-  eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow-night.sh
+  eval sh $HOME/.config/base16-shell/scripts/base16-eighties.sh
 end
 
 # Disable greeting.
@@ -91,23 +91,64 @@ end
 #######################
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles-repo/ --work-tree=$HOME'
 config config status.showUntrackedFiles no
-set fish_color_autosuggestion 505050 fda331
-set fish_color_command 6fb3d2 d381c3
-set fish_color_comment fb0120
-set fish_color_cwd a1c659
-set fish_color_cwd_root fb0120
-set fish_color_error fb0120 --bold
-set fish_color_escape 76c7b7
-set fish_color_history_current 76c7b7
-set fish_color_match 76c7b7
-set fish_color_normal e0e0e0
-set fish_color_operator 76c7b7
-set fish_color_param 6fb3d2 76c7b7
-set fish_color_quote b3643c
-set fish_color_redirection e0e0e0
-set fish_color_search_match --background=303030
-set fish_color_selection --background=303030
-set fish_pager_color_completion e0e0e0
-set fish_pager_color_description 505050 fda331
-set fish_pager_color_prefix 76c7b7
-set fish_pager_color_progress 76c7b7
+
+
+# set -l base03  "--bold black"
+# set -l base02  "black"
+# set -l base01  "--bold green"
+# set -l base00  "--bold yellow"
+# set -l base0   "--bold blue"
+# set -l base1   "--bold cyan"
+# set -l base2   "white"
+# set -l base3   "--bold white"
+# set -l yellow  "yellow"
+# set -l orange  "--bold red"
+# set -l red     "red"
+# set -l magenta "magenta"
+# set -l violet  "--bold magenta"
+# set -l blue    "blue"
+# set -l cyan    "cyan"
+# set -l green   "green"
+
+
+# set fish_color_autosuggestion 505050 fda331
+# set fish_color_command 6fb3d2 d381c3
+# set fish_color_comment fb0120
+# set fish_color_cwd a1c659
+# set fish_color_cwd_root fb0120
+# set fish_color_error fb0120 --bold
+# set fish_color_escape 76c7b7
+# set fish_color_history_current 76c7b7
+# set fish_color_match 76c7b7
+# set fish_color_normal e0e0e0
+# set fish_color_operator 76c7b7
+# set fish_color_param 6fb3d2 76c7b7
+# set fish_color_quote b3643c
+# set fish_color_redirection e0e0e0
+# set fish_color_search_match --background=303030
+# set fish_color_selection --background=303030
+# set fish_pager_color_completion e0e0e0
+# set fish_pager_color_description 505050 fda331
+# set fish_pager_color_prefix 76c7b7
+# set fish_pager_color_progress 76c7b7
+
+
+
+set -g fish_color_normal      $base0
+set -g fish_color_command     $base0
+set -g fish_color_quote       $cyan
+set -g fish_color_redirection $base0
+set -g fish_color_end         $base0
+set -g fish_color_error       $red
+set -g fish_color_param       $blue
+set -g fish_color_comment     $base01
+set -g fish_color_match       $cyan
+set -g fish_color_search_match "--background=$base02"
+set -g fish_color_operator    $orange
+set -g fish_color_escape      $cyan
+
+# Used by fish_prompt
+
+set -g fish_color_hostname    $cyan
+set -g fish_color_cwd         $yellow
+set -g fish_color_git         $green
